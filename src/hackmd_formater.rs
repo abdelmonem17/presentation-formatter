e866a53999cf17fb96b4e3445284a21a09a8b0bc
Your Name <abdelmonem.mostafa55@gmail.com>
2022-02-09 01:26:53 +0200

    create the initial project

---
+++ src/hackmd_formater.rs
@@ -0,0 +1,40 @@
+use crate::{FormatWriter, UserData, Section};
+use std::io::Write;
+use std::error::Error;
+
+///
+pub struct HackMDFormatter;
+
+impl FormatWriter for HackMDFormatter {
+    fn writer(&self,path: &str,data:&UserData) ->Result<(),Box<dyn Error>>{
+        let mut file_content = String::new();
+        if !data.title.is_empty(){
+            file_content = file_content +"#"+ data.title.as_str() + "\n";
+        }
+
+        let mut file = std::fs::File::create(path)?;
+        file_content.push_str(&*format_sections_for_hackmd(&data.sections));
+        file.write_all(file_content.as_bytes())?;
+        Ok(())
+    }
+}
+
+fn format_sections_for_hackmd(sections:&Vec<Section>)->String{
+    let mut data = String::new();
+    for section in sections{
+        if !section.title.is_empty(){
+            data = data + "#" + section.title.as_str() + "\n";
+        }
+        if !section.intro.is_empty(){
+            data = data + section.intro.as_str() + "\n";
+        }
+
+        if !section.code.is_empty(){
+            data = data +"```rust=\n"+section.code.as_str() + "\n```\n";
+        }
+        if !section.details.is_empty(){
+            data = data + section.details.as_str() + "\n";
+        }
+    }
+    data
+}
