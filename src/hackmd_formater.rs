5915edc80cb7d7f1e83f155bb09915c7e41b09dd
Your Name <abdelmonem.mostafa55@gmail.com>
2022-02-09 13:00:16 +0200

    handle sub header

--- src/hackmd_formater.rs
+++ src/hackmd_formater.rs
@@ -9,7 +9,7 @@ impl FormatWriter for HackMDFormatter {
     fn writer(&self,path: &str,data:&UserData) ->Result<(),Box<dyn Error>>{
         let mut file_content = String::new();
         if !data.title.is_empty(){
-            file_content = file_content +"#"+ data.title.as_str() + "\n";
+            file_content = file_content +"# "+ data.title.as_str() + "\n";
         }
 
         let mut file = std::fs::File::create(path)?;
@@ -23,7 +23,7 @@ fn format_sections_for_hackmd(sections:&Vec<Section>)->String{
     let mut data = String::new();
     for section in sections{
         if !section.title.is_empty(){
-            data = data + "#" + section.title.as_str() + "\n";
+            data = data + "## " + section.title.as_str() + "\n";
         }
         if !section.introduction.is_empty(){
             data = data + section.introduction.as_str() + "\n";
